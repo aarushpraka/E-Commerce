5a3bef81ce6c2393a902fbc91069b0890955c620
Aarush Prakash <164503946+aarushpraka@users.noreply.github.com>
2024-12-09 20:50:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+//import {Routes,Route} from 'react-router-dom';
+//import Home from './pages/Home';
+//import About from './pages/About';
+//import Orders from './pages/Orders';
+//import Login from './pages/login';
+//import Contact from './pages/contact';
+//import Cart from './pages/cart';
+//import Collection from './pages/Collection';
+//import Product from './pages/product';
+import Navbar from './componets/Navbar';
+//import PlaceOrder from './pages/placeorder';
+
+
+const App = () => {
+  return (
+    <div className='px-4 s,:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <Navbar />
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { assets } from '../assets/assets';
+
+const Hero = () => {
+  const slides = [
+    { src: assets.banner1 },
+    { src: assets.banner2 },
+    { src: assets.banner3 },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+      <div
+        style={{
+          backgroundImage: `url(${slides[currentIndex].src})`, // Corrected here
+        }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+
+      {/* Previous Button */}
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-8 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg hover:bg-gray-300"
+      >
+        ❮
+      </button>
+
+      {/* Next Button */}
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-8 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg hover:bg-gray-300"
+      >
+        ❯
+      </button>
+    </div>
+  );
+};
+
+export default Hero
